d4f831525ce060e7ba5d526f0df5531ad798ee57
galshore <g.shore@campus.technion.ac.il>
2021-05-11 23:10:05 +0300

    intial back for game history

---
+++ game_history.c
@@ -0,0 +1,18 @@
+//
+// Created by DELL on 11/05/2021.
+//
+
+#include "game_history.h"
+
+
+struct Map_t{
+    Pair* pairs;
+    int current_size;
+    int maxSize;
+    int iterator;
+    copyMapDataElements copyDataElement;
+    copyMapKeyElements copyKeyElement;
+    freeMapDataElements freeDataElement;
+    freeMapKeyElements freeKeyElement;
+    compareMapKeyElements compareKeyElements;
+};
